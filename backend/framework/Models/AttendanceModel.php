b24713137bcda07991b430b81ff79fb8b85d8e6f
Calil Christopher Jaudian <calilchristopher052997@gmail.com>
2022-04-03 21:29:44 +0800

    created attendance model and controller

--- backend/framework/Models/AttendanceModel.php
+++ backend/framework/Models/AttendanceModel.php
@@ -5,9 +5,52 @@ require_once '../../autoload.php';
  class AttendanceModel {
      private $base_table = 'bro_attendance';
 
-     public function create_attendance()
+     public function create_attendance($payload = [])
      {
         global $db, $common;
 
+        $arr = [
+            "sunday_date" => $payload['sunday_date'],
+            "account_pk"  => $payload['account_pk'],
+        ];
+
+        $fields = $common->get_insert_fields($arr);
+        return $db->insert("INSERT INTO {$this->base_table} {$fields}", array_values($arr));
+     }
+
+    //  GET INFORMATION OF SINGLE ATTENDANCE
+     public function get_attendance($id = null)
+     {
+        global $db, $common;
+
+        return $db->get_row("SELECT * FROM {$this->base_table} WHERE id = ?", [$id]);
+     }
+ 
+    //  ATTENDANCE LIST
+     public function get_attendance_list($user_id = null)
+     {
+        global $db, $common;
+
+        return $db->get_list("SELECT * FROM {$this->base_table} WHERE account_pk = ?", [$user_id]);
+     }
+ 
+     public function update_attendance($payload = [], $id = null)
+     {
+        global $db, $common;
+
+        $update_fields = $common->get_update_fields($payload);
+        $updated       = $db->update("UPDATE {$this->base_table} {$update_fields} WHERE id = {$id}", array_values($payload));
+
+        return $updated ? $this->get_attendance($id) : false;
+     }
+
+     public function remove_attendance($id = null)
+     {
+        global $db, $common;
+        $tobe_deleted = $this->get_attendance($id);
+        $deleted      = $db->delete("DELETE FROM {$this->base_table} WHERE id = {$id}");
+        return $deleted ? $tobe_deleted : false;
      }
  }
+
+ $attendance_model = new AttendanceModel();
---
+++ backend/framework/controllers/AttendanceController.php
@@ -0,0 +1,60 @@
+<?php
+include_once '../models/AttendanceModel.php';
+
+$act = !empty($_GET['action']) ? $_GET['action'] : '';
+
+switch ($act) {
+    case 'create_attendance':
+       echo json_encode(
+           $common->create_response(
+               'AttendanceController.php/?action=create_attendance',
+               $attendance_model->create_attendance($_POST),
+               1
+            )
+       );
+        break;
+    
+    case 'update_attendance':
+       echo json_encode(
+           $common->create_response(
+               'AttendanceController.php/?action=update_attendance',
+               $attendance_model->update_attendance($_POST, $_GET['id']),
+               1
+            )
+       );
+        break;
+    
+    case 'remove_attendance':
+       echo json_encode(
+           $common->create_response(
+               'AttendanceController.php/?action=remove_attendance',
+               $attendance_model->remove_attendance($_GET['id']),
+               1
+            )
+       );
+        break;
+    
+    case 'get_attendance_details':
+       echo json_encode(
+           $common->create_response(
+               'AttendanceController.php/?action=get_attendance_details',
+               $attendance_model->get_attendance($_GET['id']),
+               1
+            )
+       );
+        break;
+    
+    case 'get_attendance_list':
+       echo json_encode(
+           $common->create_response(
+               'AttendanceController.php/?action=get_attendance_details',
+               $attendance_model->get_attendance_list($_GET['user_id']),
+               1
+            )
+       );
+        break;
+    
+    default:
+        # code...
+        break;
+}
